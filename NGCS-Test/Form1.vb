73a8a8e26f6ee3252aaf64907bba8ed5721bf564
Tim Garrity <timgarrity89@gmail.com>
2015-10-15 13:40:14 -0400

    Initial creation of NGCS class with a working WinForm. Later this will be changed to just a ngcs library

---
+++ NGCS-Test/Form1.vb
@@ -0,0 +1,8 @@
+﻿Imports NGCS_Test
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ngcs As New NGCS_Test.ngcs("YOUR TOKEN")
+        Console.WriteLine(ngcs._servers.getServers()) 'Get all Servers
+        Console.WriteLine(ngcs._servers.getServer("217780B2E8BB1112BC39F6109E1D02C0")) 'Get Server By ID
+    End Sub
+End Class
---
+++ NGCS-Test/ngcs.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text
+Imports System.Web.Script.Serialization
+
+Public Class ngcs
+    Protected Property _api As String
+    Public Sub New(ByVal apikey As String)
+        _api = apikey
+    End Sub
+    Public Function _servers() As Object
+        Return New Servers(_api)
+    End Function
+End Class
+Public Class Servers
+    Inherits ngcs
+    Dim url As String = “https://cloudpanel-api.1and1.com/v1/”
+    Sub New(ByVal api As String)
+        MyBase.New(api)
+    End Sub
+    Function getServers()
+        'Create the request
+        Dim _command As String = url + "servers"
+        Dim request As HttpWebRequest = WebRequest.Create(_command)
+        request.Method = "GET"
+        request.Headers.Add("X-TOKEN", MyBase._api)
+
+        'Call API
+        Try
+            Dim response As HttpWebResponse
+            response = DirectCast(request.GetResponse, HttpWebResponse)
+            Dim reader As New StreamReader(response.GetResponseStream)
+            Return (reader.ReadToEnd)
+        Catch e As WebException
+            Return ("Status code: " + Convert.ToString(CType(e.Response, HttpWebResponse).StatusCode) + vbCrLf + "Message: " + e.Message + vbCrLf + request.Headers.ToString)
+        End Try
+
+    End Function
+
+    Function getServer(ByVal serverID As String)
+        'Create the request
+        Dim _command As String = url + "servers/" + serverID
+        Dim request As HttpWebRequest = WebRequest.Create(_command)
+        request.Method = "GET"
+        request.Headers.Add("X-TOKEN", MyBase._api)
+
+        'Call API
+        Try
+            Dim response As HttpWebResponse
+            response = DirectCast(request.GetResponse, HttpWebResponse)
+            Dim reader As New StreamReader(response.GetResponseStream)
+            Return (reader.ReadToEnd)
+        Catch e As WebException
+            Return ("Status code: " + Convert.ToString(CType(e.Response, HttpWebResponse).StatusCode) + vbCrLf + "Message: " + e.Message + vbCrLf + request.Headers.ToString)
+        End Try
+    End Function
+
+End Class
+
