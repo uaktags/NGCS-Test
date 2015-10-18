dc40c31b8c29c31be2e94289902dc8f93fed0442
Tim Garrity <timgarrity89@gmail.com>
2015-10-18 14:06:54 -0400

    Few updates

--- NGCS-Test/Form1.vb
+++ NGCS-Test/Form1.vb
@@ -1,8 +1,14 @@
 ﻿Imports NGCS_Test
+Imports System.Web.Script.Serialization
+
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim ngcs As New NGCS_Test.ngcs("YOUR TOKEN")
+        Dim ngcs As New NGCS_Test.ngcs(My.Settings.apikey)
         Console.WriteLine(ngcs._servers.getServers()) 'Get all Servers
-        Console.WriteLine(ngcs._servers.getServer("217780B2E8BB1112BC39F6109E1D02C0")) 'Get Server By ID
+        Dim dict = New JavaScriptSerializer().Deserialize(Of List(Of ServerEntity))(ngcs._servers.getServers())
+        For Each i In dict
+            Console.WriteLine(ngcs._servers.getServer(i.id)) 'Write each Server out.
+        Next
+
     End Sub
 End Class
--- NGCS-Test/ngcs.vb
+++ NGCS-Test/ngcs.vb
@@ -57,3 +57,57 @@ Public Class Servers
 
 End Class
 
+Public Class ServerEntity
+    Private _ID As String
+    Private _name As String
+    Private _harddrives As List(Of Object)
+    Private _hardware As hardwareEntity
+    Public Property id() As String
+        Get
+            Return _ID
+        End Get
+        Set(value As String)
+            _ID = value
+        End Set
+    End Property
+    Public Property name() As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property hdds() As List(Of Object)
+        Get
+            Return _harddrives
+        End Get
+        Set(value As List(Of Object))
+            _harddrives = value
+        End Set
+    End Property
+
+    Public Property hardware As hardwareEntity
+        Get
+            Return _hardware
+        End Get
+        Set(value As hardwareEntity)
+            _hardware = value
+        End Set
+    End Property
+End Class
+
+Public Class hardwareEntity
+    Private _hdds As List(Of Object)
+    Public Property hdds As Object
+        Get
+            Return _hdds
+        End Get
+        Set(value As Object)
+            +
+            _hdds.Add(value)
+        End Set
+    End Property
+End Class
+
